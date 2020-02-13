ac2ff1aed81ff58fcf4ff91969851c2daae36c91
Jesús López Corominas <jesuslcorominas@gmail.com>
2020-02-13 23:53:50 +0100

    Agregados test de BooksRepository

---
+++ testShared/src/main/java/com/jesuslcorominas/mybooks/testshared/mocks.kt
@@ -0,0 +1,15 @@
+package com.jesuslcorominas.mybooks.testshared
+
+import com.jesuslcorominas.mybooks.domain.Book
+
+val mockedBook = Book(
+    0,
+    "uOZhYgEACAAJ",
+    "El nombre del viento",
+    "http://books.google.com/books/content?id=uOZhYgEACAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE71YUyCqpSGt9VwfymN58u_02LrrelqTh8JMoMYVSoYVkBv3ws0jrOoBzTXVN-Q6IO9Ubmbjsz4ybygAJiaJdZCMguw6lKnbMeAvbCaGIURsb3MLrq1SMUJqmmx2Sdpkdxb0OUGW&source=gbs_api",
+    "Descripcion de ejemplo",
+    "https://books.google.es/books?id=uOZhYgEACAAJ&hl=es&source=gbs_api",
+    false,
+    false,
+    false
+)
